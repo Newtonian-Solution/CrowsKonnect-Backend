a64f839bf081580149c1fd11dfe90d005918cb88
Nurudeen Ahmed <ahmednurudeen06@gmail.com>
2024-05-24 17:20:20 +0100

    remove limiter

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const limiter = rateLimit({
     windowMs: 60 * 60 * 1000,
     message: 'Too Many Request from this IP, please try again in an hour'
 });
-app.use('/api', limiter);
+// app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '25mb', extended: true }));
